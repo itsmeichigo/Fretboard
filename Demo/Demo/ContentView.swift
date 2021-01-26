54c4c45d3ca8c7f1212a6b3affcb521f4e460166
Huong Do <huongdt29@gmail.com>
2021-01-26 23:14:21 +0700

    Cooler demo

--- Demo/Demo/ContentView.swift
+++ Demo/Demo/ContentView.swift
@@ -9,17 +9,59 @@ import SwiftUI
 import Fretboard
 
 struct ContentView: View {
+    @State private var selectedKey: GuitarChord.Key = .c
+    @State private var selectedSuffix: GuitarChord.Suffix = .major
+    
+    private let allChords: [GuitarChord]
+    private let allKeys: [GuitarChord.Key]
+    private let allSuffixes: [GuitarChord.Suffix]
+    
+    private var columns: [GridItem] =
+             Array(repeating: .init(.flexible()), count: 4)
+    private var foundChords: [GuitarChord] {
+        allChords.matching(key: selectedKey)
+            .matching(suffix: selectedSuffix)
+    }
+    
+    init() {
+        allChords = GuitarChord.all
+        allKeys = Array(Set(allChords.keys))
+        allSuffixes = Array(Set(allChords.suffixes))
+    }
+    
     var body: some View {
-        VStack {
-            Text("C Major")
-                .font(.headline)
-            
-            FretView(fingers: [0, 3, 2, 0, 1, 0],
-                     strings: [-1, 3, 2, 0, 1, 0],
-                     barres: [])
-                .frame(width: 100, height: 200)
+        ScrollView {
+            VStack {
+                Text("Guitar Chord Lookup 🎸")
+                    .font(.largeTitle)
+                    .padding()
+                
+                HStack {
+                    Picker("Select key", selection: $selectedKey) {
+                        ForEach(allKeys, id: \.self) {
+                            Text($0.rawValue)
+                        }
+                    }
+                    
+                    Picker("Select suffix", selection: $selectedSuffix) {
+                        ForEach(allSuffixes, id: \.self) {
+                            Text($0.rawValue)
+                        }
+                    }
+                }
+                
+                LazyVGrid(columns: columns) {
+                    ForEach(foundChords, id: \.self) { chord in
+                        FretView(chord: chord)
+                            .frame(width: 100, height: 200)
+                    }
+                }
+                
+                Spacer()
+            }
         }
-        
+        .frame(width: 500, height: 300)
+        .padding()
     }
 }
 
